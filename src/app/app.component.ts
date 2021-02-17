1068c7e8414f76720564adbe2da2e7775e5af6b6
dominikk19 <dominikk19@gmail.com>
2021-02-17 19:47:11 +0100

    refactor display of quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {QUOTES} from './models/database';
+import {Quotation} from './models/quotation';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'best-quotes';
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author: '', sentence: '', votes: 0};
+  }
 }
